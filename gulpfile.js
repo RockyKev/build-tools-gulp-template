2da032f943696f58d0279e56fbeec1906ac050cf
rocky kev <kev.rocky@gmail.com>
2022-12-05 23:00:51 -0800

    compiled

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,41 @@
 "use strict";
 
-const gulp = require("gulp");
+const autoprefixer = require("autoprefixer");
+const babel = require('gulp-babel');
 const concat = require("gulp-concat");
+const csso = require("gulp-csso");
+const del = require("del");
+const gulp = require("gulp");
+const imagemin = require("gulp-imagemin");
+const include = require("posthtml-include");
 const plumber = require("gulp-plumber");
+const postcss = require("gulp-postcss");
+const posthtml = require("gulp-posthtml");
+const rename = require("gulp-rename");
 const sourcemap = require("gulp-sourcemaps");
 const sass = require('gulp-sass')(require('sass'));
-const postcss = require("gulp-postcss");
-const autoprefixer = require("autoprefixer");
 const server = require("browser-sync").create();
-const csso = require("gulp-csso");
-const rename = require("gulp-rename");
-const imagemin = require("gulp-imagemin");
-const webp = require("gulp-webp");
 const svgstore = require("gulp-svgstore")
-const posthtml = require("gulp-posthtml");
-const include = require("posthtml-include");
-const del = require("del");
+const webp = require("gulp-webp");
+const uglify = require('gulp-uglify');
 
-/** Scenario 1 - combine files */
 gulp.task('js', function() {
   return gulp.src('src/js/*.js')
-  .pipe(concat('index.js'))
+  .pipe(concat('index.js')) //  Scenario 1 - combine files
+  .pipe(babel({presets: ['@babel/env']})) // Scenario 4 - Babel
+  .pipe(uglify()) // Scenario 2 - Minify & Obfuscate
   .pipe(rename({suffix: '.min'}))
   .pipe(gulp.dest('asset/js'))
 })
+
+
 
 gulp.task("css", function () {
   return gulp.src("src/sass/style.scss")
     .pipe(plumber())
     .pipe(sourcemap.init())
     .pipe(sass())
-    .pipe(postcss([ autoprefixer() ]))
+    .pipe(postcss([ autoprefixer() ])) // Scenario 3 - Vendor Prefixes
     .pipe(csso())
     .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
